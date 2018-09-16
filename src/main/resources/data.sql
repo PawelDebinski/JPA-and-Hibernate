0d36fbaa652a8dc8ba0174e39c6dc45830d1d55d
Pawel Debinski <spokojasne@interia.eu>
2018-09-16 20:16:47 +0200

    Added Review entity, closes #15

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,3 +9,7 @@ insert into student(id, name) values(20003, 'Jane');
 insert into passport(id, number) values(40001, 'E234567');
 insert into passport(id, number) values(40002, 'B883332');
 insert into passport(id, number) values(40003, 'L123412');
+
+insert into review(id, rating, description) values(50001, '5', 'Great Course');
+insert into review(id, rating, description) values(50002, '4', 'Wonderful Course');
+insert into review(id, rating, description) values(50003, '5', 'Awesome Course');
