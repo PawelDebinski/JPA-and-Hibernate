42ad02e9207ee0b1d149b5bf6a622e2153165ae7
Pawel Debinski <spokojasne@interia.eu>
2018-09-16 19:27:00 +0200

    Added Student entity, closes #13

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,7 @@
 insert into course(id, name, created_date, last_updated_date) values(10001, 'JPA in 50 Steps', sysdate(), sysdate());
 insert into course(id, name, created_date, last_updated_date) values(10002, 'Spring in 50 Steps', sysdate(), sysdate());
 insert into course(id, name, created_date, last_updated_date) values(10003, 'Spring Boot in 50 Steps', sysdate(), sysdate());
+
+insert into student(id, name) values(20001, 'Ranga');
+insert into student(id, name) values(20002, 'Adam');
+insert into student(id, name) values(20003, 'Jane');
