57a1b59001a691141dda5b9e8c73971d56876952
Pawel Debinski <spokojasne@interia.eu>
2018-09-21 10:28:15 +0200

    Spring Data Jpa - save and findAll, closes #40

--- src/test/java/pl/pawel/dbdemo/repository/CourseSpringDataRepositoryTest.java
+++ src/test/java/pl/pawel/dbdemo/repository/CourseSpringDataRepositoryTest.java
@@ -35,4 +35,16 @@ public class CourseSpringDataRepositoryTest {
         assertFalse(courseOptional.isPresent());
     }
 
+    @Test
+    public void playingAroundWithSpringDataRepository() {
+        Course course = new Course("Microservices in 100 Steps");
+        repository.save(course);
+
+        course.setName("Microservices in 100 Steps - Update");
+        repository.save(course);
+
+        logger.info("Courses -> {} ", repository.findAll());
+        logger.info("Count -> {} ", repository.count());
+    }
+
 }
