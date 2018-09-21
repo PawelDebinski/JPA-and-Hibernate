0cf3b5b722d4e620a6569cd621c4d3bbb4880059
Pawel Debinski <spokojasne@interia.eu>
2018-09-21 11:05:54 +0200

    Spring Data Jpa - pagination, closes #42

--- src/test/java/pl/pawel/dbdemo/repository/CourseSpringDataRepositoryTest.java
+++ src/test/java/pl/pawel/dbdemo/repository/CourseSpringDataRepositoryTest.java
@@ -6,6 +6,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -54,6 +57,18 @@ public class CourseSpringDataRepositoryTest {
         logger.info("Sorted Courses -> {} ", repository.findAll(sort));
 
         logger.info("Count -> {} ", repository.count());
+    }
+
+    @Test
+    public void pagination() {
+        PageRequest pageRequest = PageRequest.of(0, 3);
+        Page<Course> firstPage = repository.findAll(pageRequest);
+        logger.info("First Page -> {} ", firstPage.getContent());
+
+        Pageable secondPageable = firstPage.nextPageable();
+        Page<Course> secondPage = repository.findAll(secondPageable);
+        logger.info("Second Page -> {} ", secondPage.getContent());
+
     }
 
 }
